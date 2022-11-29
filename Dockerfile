d04523db87068f4fdf84aede1a9216aa4cee5fbb
nitinjainsj <nitin@pachyderm.com>
2022-11-28 21:03:17 -0800

    Use install script

--- Dockerfile
+++ Dockerfile
@@ -26,41 +26,25 @@ LABEL org.opencontainers.image.title="Pach extension" \
     com.docker.extension.changelog=""
 
 RUN apk add curl
-RUN curl -L -o helm-linux-amd64.tar.gz https://get.helm.sh/helm-v3.10.2-linux-amd64.tar.gz \
-    && mkdir /linux \
-    && tar -zxvf helm-linux-amd64.tar.gz \
-    && chmod +x linux-amd64/helm && mv linux-amd64/helm /linux
-
-RUN curl -L -o helm-darwin-amd64.tar.gz https://get.helm.sh/helm-v3.10.2-darwin-amd64.tar.gz \
-    && mkdir /darwin \
-    && tar -zxvf helm-darwin-amd64.tar.gz \
-    && chmod +x darwin-amd64/helm && mv darwin-amd64/helm /darwin
-
-RUN curl -L -o helm-windows-amd64.tar.gz https://get.helm.sh/helm-v3.10.2-windows-amd64.tar.gz \
-    && mkdir /windows \
-    && tar -zxvf helm-windows-amd64.tar.gz \
-    && chmod +x windows-amd64/helm.exe && mv windows-amd64/helm.exe /windows
-
 RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl \
+    && mkdir /linux \
     && chmod +x ./kubectl && mv ./kubectl /usr/local/bin/kubectl \
     && cp /usr/local/bin/kubectl /linux/
 
 RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/amd64/kubectl" \
+    && mkdir /darwin \
     && chmod +x ./kubectl && mv ./kubectl /darwin/
 
 RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/windows/amd64/kubectl.exe" \
+    && mkdir /windows \
     && chmod +x ./kubectl.exe && mv ./kubectl.exe /windows/
 
-RUN apk add unzip
-RUN curl -LO https://github.com/pachyderm/pachyderm/releases/download/v2.4.0/pachctl_2.4.0_darwin_amd64.zip \
-    && unzip pachctl_2.4.0_darwin_amd64.zip \
-    && chmod +x pachctl_2.4.0_darwin_amd64/pachctl \
-    && mv pachctl_2.4.0_darwin_amd64/pachctl /darwin/
+COPY --chmod=0755 script/install-darwin.sh /darwin/install
+COPY --chmod=0755 script/install-linux.sh /linux/install
+COPY --chmod=0755 script/install-windows.ps1 /windows/install
+COPY --chmod=0755 script/install-linux.sh /windows/iinstall-linux.sh
 
-RUN curl -LO https://github.com/pachyderm/pachyderm/releases/download/v2.4.0/pachctl_2.4.0_linux_amd64.tar.gz \
-    && tar -zxvf pachctl_2.4.0_linux_amd64.tar.gz \
-    && chmod +x pachctl_2.4.0_linux_amd64/pachctl \
-    && mv pachctl_2.4.0_linux_amd64/pachctl /linux/
+
 
 COPY metadata.json .
 COPY docker.svg .
--- ui/src/helper/kubernetes.ts
+++ ui/src/helper/kubernetes.ts
@@ -7,7 +7,7 @@ export const IsK8sEnabled = "isK8sEnabled";
 export const updatePach = async (
   ddClient: v1.DockerDesktopClient
 ) => {
-    let result = "Paste the following commands in your terminal.\necho '{\"pachd_address\":\"grpc://127.0.0.1:80\"}' | pachctl config set context local --overwrite && pachctl config set active-context local";
+    let result = "Go to http://localhost";
     try {
         let output = await ddClient.extension.host?.cli.exec("kubectl", [
             "cluster-info",
@@ -20,70 +20,13 @@ export const updatePach = async (
     }
     console.log("Kubernetes enabled\n");
     try {
-        let out = await ddClient.extension.host?.cli.exec("helm", [
-            "uninstall",
-            "pachd",
-        ]);
-    } catch (e: any) {
-        if (e.stderr !== "Error: uninstall: Release not loaded: pachd: release: not found\n") {
-            console.error(e);
-            return e?.stderr;
-        }
-    }
-    console.log("Pach install clean\n");
-    try {
-        let out = await ddClient.extension.host?.cli.exec("helm", [
-            "repo",
-            "add",
-            "pach",
-            "https://helm.pachyderm.com",
-        ]);
-    } catch (e: any) {
-        if (e.stderr !== "Error: repository name (pach) already exists, please specify a different name\n") {
-            console.error(e);
-            return e?.stderr;
-        }
-    }
-    console.log("Helm add repo (pach)\n");
-    try {
-        let out = await ddClient.extension.host?.cli.exec("helm", [
-            "repo",
-            "update",
-            "pach",
+        let output = await ddClient.extension.host?.cli.exec("install", [
+            "/windows/install-linux.sh",
         ]);
     } catch (e: any) {
         console.error(e);
         return e?.stderr;
     }
-    console.log("Helm update repo (pach)\n");
-    try {
-        let out = await ddClient.extension.host?.cli.exec("helm", [
-            "install",
-            "pachd",
-            "pach/pachyderm",
-            "--set",
-            "deployTarget=LOCAL",
-            "--set",
-            "proxy.enabled=true",
-            "--set proxy.service.type=LoadBalancer",
-            "--set pachd.clusterDeploymentID=my-personal-pachyderm-deployment",
-        ]);
-    } catch (e: any) {
-        console.error(e);
-        return e?.stderr;
-    }
-    console.log("Pachyderm installed\n");
-    try {
-        let out = await ddClient.extension.host?.cli.exec("pachctl", [
-            "config",
-            "set",
-            "active-context",
-            "local",
-        ]);
-    } catch (e: any) {
-        console.error(e);
-        return e?.stderr;
-    }
-    console.log("Pachyderm context set to local\n");
+    console.log("Pachyderm installed and context set to local\n");
     return result;
 };
