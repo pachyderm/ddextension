2003bc396b51b31dc71a6ad42ddc04ca2d637234
nitinjainsj <nitin@pachyderm.com>
2022-12-06 12:37:48 -0800

    Fix some permission issues with copying pachctl

--- script/install.sh
+++ script/install.sh
@@ -23,45 +23,37 @@ copyPachctl() {
         curl -Ls -o pachctl.zip ${URL} > /dev/null 2>&1
         unzip pachctl.zip > /dev/null 2>&1
         chmod +x pachctl_${PACHCTL_VER}_darwin_${ARCH}/pachctl
-        cp pachctl_${PACHCTL_VER}_darwin_${ARCH}/pachctl $1
-        rm -rf pachctl_${PACHCTL_VER}_darwin_${ARCH} pachctl.zip
+        mv pachctl_${PACHCTL_VER}_darwin_${ARCH} pachctl_dir
+        rm -f pachctl.zip
     elif [ "${MACH}" == "Linux" ] || [ "${MACH}" == "linux" ]; then
         URL=https://github.com/pachyderm/pachyderm/releases/download/v$PACHCTL_VER/pachctl_${PACHCTL_VER}_linux_${ARCH}.tar.gz
         curl -Ls -o pachctl.tar.gz ${URL} > /dev/null 2>&1
         tar -zxf pachctl.tar.gz
         chmod +x pachctl_${PACHCTL_VER}_linux_${ARCH}/pachctl
-        cp pachctl_${PACHCTL_VER}_linux_${ARCH}/pachctl $1
-        rm -rf pachctl_${PACHCTL_VER}_linux_${ARCH} pachctl.tar.gz
+        mv pachctl_${PACHCTL_VER}_linux_${ARCH} pachctl_dir
+        rm -f pachctl.tar.gz
     else
         abort "Cannot find ${MACH} for ${ARCH}"
     fi
-}
 
-forceInstallPachctl() {
-    PACHCTL_INSTALL_PATH="$(command -v pachctl)"
-    if [[ -z "${PACHCTL_INSTALL_PATH}" ]]; then
-        if [[ "$(touch /usr/local/bin/pachctl)" ]]; then
-            IFS=: read -r -d '' -a path_array < <(printf '%s:\0' "${PATH}")
-            set -o noglob
-            for p in "${path_array[@]}"; do
-                P_PATH="$p/pachctl"
-                if [[ -z "$(touch "${P_PATH}")" ]]; then
-                    PACHCTL_INSTALL_PATH="${P_PATH}"
-                    break
-                fi
-            done
-        else
-            PACHCTL_INSTALL_PATH="/usr/local/bin/pachctl"
-        fi
-        rm -f "${PACHCTL_INSTALL_PATH}"
+    if [[ "$(cp -f pachctl_dir/pachctl /usr/local/bin/pachctl)" -ne 0 ]]; then
+        IFS=: read -r -d '' -a path_array < <(printf '%s:\0' "${PATH}")
+        set -o noglob
+        for p in "${path_array[@]}"; do
+            P_PATH="$p/pachctl"
+            if [[ "$(cp -f pachctl_dir/pachctl ${P_PATH})" -eq 0 ]]; then
+		echo "COPY file to ${P_PATH}"
+                break
+            fi
+        done
     fi
-    copyPachctl ${PACHCTL_INSTALL_PATH}
+    rm -rf pachctl_dir
 }
 
 installPachctl() {
     echo -n "Pachctl..."
     if [[ $(command -v brew) == "" ]] ; then
-        forceInstallPachctl
+        copyPachctl
         echo "installed"
     else
         if [[ $(command -v pachctl) == "" ]] ; then
